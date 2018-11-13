47a746ebb42581fa068ee61e2c3c3aa4fd4914eb
Sara Jakša <sarajaksa@gmail.com>
2018-11-13 23:25:00 +0100

    Added the arguments for the command line

--- subtitles.py
+++ subtitles.py
@@ -1,20 +1,33 @@
 import subprocess
+import argparse
 import os
 
-filename = "Flash-Season1-Episode1-34:20-37:07-FlashesFirstFight.srt"
-add_to_name = "Flash-Season1-Episode1-"
-video = "Flash-Season1-Episode1.mkv"
-filename_output = "Anki.csv"
-series = "Flash"
-season = "2"
-episode = "1"
+parser = argparse.ArgumentParser(description="Command line app to change the video and subtitles into csv file to import in Anki")
+parser.add_argument("-v", "--video", help="The video file")
+parser.add_argument("-s", "--sub", help="The subtitles file")
+parser.add_argument("-o", "--output", help="The name of output file - the one to be imported to Anki")
+parser.add_argument("--prefix", help="The prefix to be used when generating the names of pictures and sound")
+parser.add_argument("--name", help="The name of video to be added as field - like name of series or movie")
+parser.add_argument("--season", help="The season of series - to be added to Anki as field")
+parser.add_argument("--episode", help="The episode of the season of series - to be added to Anki as field")
+args = parser.parse_args()
+
+if not args.prefix:
+    args.prefix = ""
+if not args.name:
+    args.name == ""
+if not args.season:
+    args.season = ""
+if not args.episode:
+    args.episode = ""
+if not args.output:
+    args.output = "Anki.csv"
 
 def create_video(video, start, end, name):
     subprocess.call(["ffmpeg", "-ss", start, "-to", end, "-i", video, "-b:a", "320K", "-vn", name], stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
 
 def create_picture(video, start, name):
     subprocess.call(["ffmpeg", "-ss", start, "-i", video, "-f", "image2", "-vcodec", "mjpeg", "-vframes", "1", name], stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
-
 
 def find_start_and_end_times(times):
     start, end = line.strip().split("-->")
@@ -28,52 +41,38 @@ def find_start_and_end_times(times):
 
 def write_line_to_anki(filename, audio, picture, position, line):
     with open(filename, "a") as f:
-        f.write(current_line)
-        f.write("\t")
-        f.write("[sound:")
-        f.write(audio)
-        f.write("]")
-        f.write("\t")
-        f.write("<img src='")
-        f.write(picture)
-        f.write("'>")
-        f.write("\t")
-        f.write(series)
-        f.write("\t")
-        f.write(season)
-        f.write("\t")
-        f.write(episode)
+        f.write(current_line + "\t[sound:" + audio + "]\t<img src='" + picture)
+        if args.name:
+            f.write("\t" + args.name)
+        if args.season:
+            f.write("\t" + args.season)
+        if args.episode:
+            f.write("\t" + args.episode)
         f.write("\n")
 
-with open(filename) as f:
+with open(args.sub) as f:
     lines = f.readlines()
 
 line_count = 0
-
-with open("Anki.csv", "w") as f:
-    for line in lines:
-        if line_count == 0:
-            position = int(line.strip().replace("\ufeff", ""))
-            line_count = line_count + 1
+for line in lines:
+    if line_count == 0:
+        position = int(line.strip().replace("\ufeff", ""))
+        line_count = line_count + 1
+        continue
+    if line_count == 1:
+        start, end = find_start_and_end_times(line)
+        line_count = line_count + 1
+        current_line = ""
+        continue
+    if line_count == 2:
+        if not line.strip():
+            audio = args.prefix + start.replace(":", "_") + "_" + str(position) + ".mp3"
+            picture = args.prefix + start.replace(":", "_") + "_" + str(position)  + ".jpg"
+            create_video(args.video, start, end, audio)
+            create_picture(args.video, start, picture)
+            write_line_to_anki(args.output, audio, picture, position, current_line)
+            line_count = 0
             continue
-        if line_count == 1:
-            start, end = find_start_and_end_times(line)
-            line_count = line_count + 1
-            current_line = ""
-            continue
-        if line_count == 2:
-            if not line.strip():
-                audio = add_to_name + start.replace(":", "_") + "_" + str(position) + ".mp3"
-                picture = add_to_name + start.replace(":", "_") + "_" + str(position)  + ".jpg"
-                create_video(video, start, end, audio)
-                create_picture(video, start, picture)
-                write_line_to_anki(filename_output, audio, picture, position, current_line)
-                line_count = 0
-                print(position)
-                continue
-            else:
-                current_line = current_line + " " + line.strip()
-                current_line = current_line.strip()
-
-
-
+        else:
+            current_line = current_line + " " + line.strip()
+            current_line = current_line.strip()
